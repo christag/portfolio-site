8360addcb2aed63d44a4b1df2ae31beb54f1c029
Christopher Tagliaferro <330771+christag@users.noreply.github.com>
2025-07-19 00:47:15 -0400

    strapi setup pt1

---
+++ config/middlewares.ts
@@ -0,0 +1,37 @@
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: [
+        'http://localhost:1337',
+        'http://localhost:4321',
+        'https://*.pages.dev', // Cloudflare Pages domains
+        process.env.CLIENT_URL, // Production frontend URL
+      ].filter(Boolean),
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
